b9b50c8e8135e819c5fe1505a57325b65123d93f
Lean Tavares <lean.t@live.com>
2023-06-28 19:30:06 -0300

    Update ex1.py

--- EstudosNUMPY/ex1.py
+++ EstudosNUMPY/ex1.py
@@ -1,2 +1,28 @@
+!pip install pandas 
 
-!pip install numpy
+import pandas as pd
+class Feedback:
+  def __init__(self,nota,comentario):
+    self.nota = nota
+    self.comentario = comentario
+
+class AnalisadorFeedback:
+  def __init__(self,feedbacks):
+    self.feedbacks = feedbacks
+
+
+def calcular_nps(self):
+  detratores = sum([1for feedback in self.feedbacks if feedback.nota <=6])
+  promotores = sum([1for feedback in self.feedbacks if feedback.nota >=9])
+  return (promotores - detratores) / len(self.feedbacks) * 100
+   
+
+# Leitura do arquivo
+dados = pd.read_csv('/Users/leantavares/Documents/feedbacks.csv')
+notas = dados['nota']
+
+print(f"O NPS é: {nps}")
+
+feedbacks = [Feedback(linha['nota'], linha['comentario'])   for i, linha in dados.iterrows()]
+analisador = AnalisadorFeedback(feedbacks)
+print(nps)
